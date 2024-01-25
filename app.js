656ee4562ec8d1ceeb1c5046f79860b5bf681297
Victor Birman <victorbirman@gmail.com>
2024-01-25 18:32:37 -0300

    added cards

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var jsonData;
+let jsonData;
 
 // Function to load JSON data from file
 async function loadJsonData() {
@@ -10,21 +10,60 @@ async function loadJsonData() {
   }
 }
 
+
+String.prototype.sanitize = function() {
+  return this.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+};
+
+
 // Function to search JSON data
 function searchJson() {
-  var searchInput = document.getElementById('searchInput').value.toLowerCase();
-  var searchWords = searchInput.split(/\s+/);
-  var searchResults = document.getElementById('searchResults');
+  let searchInput = document.getElementById('searchInput').value.toLowerCase();
+  console.log (searchInput)
+  let searchWords = searchInput.split(/\s+/);
+  let searchResults = document.getElementById('searchResults');
   searchResults.innerHTML = '';
 
   jsonData.forEach(function (item) {
-    if (searchWords.every(word => item.title.toLowerCase().includes(word) || item.subtitle.toLowerCase().includes(word))) {
-      var listItem = document.createElement('li');
-      listItem.textContent = item.title + ' - ' + item.subtitle;
-      searchResults.appendChild(listItem);
+    if (searchWords.every(word => item.title.toLowerCase().sanitize().includes(word.sanitize()) || item.subtitle.toLowerCase().includes(word))) {
+    
+    //  let listItem = document.createElement('li');
+    //listItem.textContent = item.title + ' - ' + item.subtitle;
+      //searchResults.appendChild(listItem);
+      searchResults.appendChild(listItemPretty (item));
     }
   });
 }
 
+
+function listItemPretty ({title, subtitle, cover, href}) {
+
+  const containerItem = document.createElement("div");
+  containerItem.classList.add ("container-item")
+  const hrefEl = document.createElement ("a")
+  hrefEl.href = "./contenido/" + href
+  const coverEl = document.createElement ("img")
+  coverEl.src = "./contenido/" + cover
+  const containerTitle = document.createElement ("div")
+  containerTitle.classList.add ("container-title")
+
+  const itemTitle = document.createElement ("p")
+  itemTitle.textContent = title
+
+  const itemSubtitle = document.createElement ("p")
+  itemTitle.textContent = subtitle
+
+    containerTitle.append(itemTitle)
+    containerTitle.append(itemSubtitle)
+
+
+  containerItem.append (hrefEl)
+  hrefEl.append(coverEl)
+  hrefEl.append(containerTitle)
+
+
+  return containerItem
+
+}
 // Call the function to load JSON data
 loadJsonData();
